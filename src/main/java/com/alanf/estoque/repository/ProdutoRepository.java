baff88a625c83eec519e81b269cff3cbbf42e906
Alan Ferreira <131145088+AlanF-Oliveira@users.noreply.github.com>
2026-04-29 17:31:01 -0300

    feat: add Produto and Categoria repositories

---
+++ src/main/java/com/alanf/estoque/repository/ProdutoRepository.java
@@ -0,0 +1,9 @@
+package com.alanf.estoque.repository;
+
+import com.alanf.estoque.entity.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+}
