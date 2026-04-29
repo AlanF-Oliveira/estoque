427b1572ddc88e3b6fd5d84e236945fc2e9ba003
Alan Ferreira <131145088+AlanF-Oliveira@users.noreply.github.com>
2026-04-29 20:35:21 -0300

    feat: add CategoriaService methods

--- src/main/java/com/alanf/estoque/mapper/CategoriaMapper.java
+++ src/main/java/com/alanf/estoque/mapper/CategoriaMapper.java
@@ -5,10 +5,14 @@ import com.alanf.estoque.dto.categoria.CategoriaResponse;
 import com.alanf.estoque.entity.Categoria;
 import org.mapstruct.Mapper;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring")
 public interface CategoriaMapper {
 
     Categoria paraEntity(CategoriaRequest request);
 
     CategoriaResponse paraDTO(Categoria categoria);
+
+    List<CategoriaResponse> paraListaDTO(List<Categoria> categorias);
 }
